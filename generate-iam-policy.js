a721cad05eda23338c0b8004435ebfb7b09ef608
Ruslan Gainutdinov <ruslan.gainutdinov@futurice.com>
2019-05-29 21:46:17 +0300

    Handle WORKFLOW_FILE better. Add docs. Only unique lambda references

--- generate-iam-policy.js
+++ generate-iam-policy.js
@@ -1,10 +1,18 @@
+/**
+ * Generates IAM policy JSON from workflow definition
+ * FIXME: Handles only Lambdas for now.
+ */
+const path = require("path")
+
 generateWorkflowPolicy = w => {
   let lambaInvoke = [];
   Object.keys(w.States).forEach(s => {
     const step = w.States[s];
     // FIXME: add more types of tasks
     if (step.Type === "Task" && step.Resource.startsWith("arn:aws:lambda:")) {
-      lambaInvoke.push(step.Resource)
+      if (!lambaInvoke.includes(step.Resource)) {
+        lambaInvoke.push(step.Resource)
+      }
     } else {
       throw new Error("Unknown step: " + step.Type + ", " + step.Resource)
     }
@@ -24,6 +32,6 @@ generateWorkflowPolicy = w => {
   return policy;
 };
 
-workflow = require("./workflow.json");
+workflow = require(process.env.WORKFLOW_FILE ? path.resolve('./', process.env.WORKFLOW_FILE) : "./workflow.json");
 
 console.log(JSON.stringify(generateWorkflowPolicy(workflow), null, 2))
